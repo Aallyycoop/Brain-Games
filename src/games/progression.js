e6ccaa436f12c2c5936e1dd54dabb20f0793d83c
Alexandra Dmitrieva <aallycoop@gmail.com>
2023-01-08 20:12:59 +0300

    tutor's remarks №2: fix progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -18,10 +18,9 @@ const startRound = () => {
   const step = getRandomNumber(1, 20);
   const progression = getProgression(start, length, step);
   const hiddenElementIndex = getRandomNumber(1, length - 1);
-  const hiddenElement = String(progression.splice(hiddenElementIndex, 1, '..'));
+  const answer = String(progression.splice(hiddenElementIndex, 1, '..'));
   const question = progression.join(' ');
-  const correctAnswer = hiddenElement;
-  return [question, correctAnswer];
+  return [question, answer];
 };
 
 const runProgression = () => runTheGame(gameRules, startRound);
