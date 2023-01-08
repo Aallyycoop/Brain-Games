5410276d8395f163cc055cba67fdda83a3fba5c1
Alexandra Dmitrieva <aallycoop@gmail.com>
2023-01-08 15:04:14 +0300

    tutor's remarks: fix calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,5 +1,4 @@
-// import readlineSync from 'readline-sync';
-import play from '../index.js';
+import runTheGame from '../index.js';
 import getRandomNumber from '../getRandomNumber.js';
 
 const calculationResult = (num1, num2, operator) => {
@@ -21,16 +20,13 @@ const gameRules = 'What is the result of the expression?';
 
 const startRound = () => {
   const operator = operators[getRandomNumber(0, operators.length - 1)];
-  const num1 = getRandomNumber(1, 101);
-  let num2 = getRandomNumber(1, 101);
-  if (operator === '*') {
-    num2 = getRandomNumber(1, 10);
-  }
+  const num1 = getRandomNumber(0, 20);
+  const num2 = getRandomNumber(0, 20);
   const question = `${num1} ${operator} ${num2}`;
   const correctAnswer = String(calculationResult(num1, num2, operator));
   return [question, correctAnswer];
 };
 
-const playCalc = () => play(gameRules, startRound);
+const runCalc = () => runTheGame(gameRules, startRound);
 
-export default playCalc;
+export default runCalc;
